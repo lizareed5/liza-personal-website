80deb76f972b9a7e6b7773ec5aad7734d1f813f3
Liza McLain <lizamclain@gmail.com>
2022-12-02 20:58:40 -0600

    add button

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textContent = "i'm liza";
 document.querySelector("body").appendChild(h2);
+
+function show() {
+    document.getElementById('fullGarden')
+        .style.display = "block";
+    document.getElementById('gardenButton')
+        .style.display = "none";
+}
